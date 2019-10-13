8cb6d9c2882973e25da95e593ed5e091382b1bb1
henriquearaujo91 <henrique_araujo91@outlook.com>
2019-10-13 10:37:37 -0300

    Salvando registro na base de dados

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-//server jss
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+// Iniciando o app
+const app = express();
+
+// Iniciando o DB
+const uri = "mongodb://192.168.0.102:27017/nodeapi";
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(uri, options);
+
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
+
+// Primeira rota
+app.get("/", (req, res) => {
+    Product.create({
+        title: 'Google',
+        description: 'Buscador',
+        url: 'https://www.google.com/',
+    }); 
+    return res.send("Chamada a api");
+});
+
+app.listen(3001);
